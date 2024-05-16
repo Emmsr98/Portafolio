d140715ff5e580915fecc21d40591c0fc46670af
Emmanuel Solis Robles <141767329+Emmsr98@users.noreply.github.com>
2024-05-16 13:28:18 -0600

    Diseño finalizado. Queda pendiente modificar experiencia

--- PORTAFOLIO/validacion.js
+++ PORTAFOLIO/validacion.js
@@ -1,1 +1,36 @@
-//Haz tú validación en javascript acá
+const inputs = document.querySelectorAll('.input');
+const inputsMsj = document.querySelectorAll('.input-padron-message');
+
+inputs.forEach(input => {
+
+    input.onfocus = () => {
+        input.previousElementSibling.classList.add('top');
+        input.previousElementSibling.classList.add('focus');
+        input.parentNode.classList.add('focus');
+    };
+
+    input.onblur = function(){
+        if(input.value.trim().length == 0){
+            input.previousElementSibling.classList.remove('top');
+        }
+        input.previousElementSibling.classList.remove('focus');
+        input.parentNode.classList.remove('focus');
+    }
+});
+
+inputsMsj.forEach(input => {
+    
+    input.onfocus = () => {
+        input.previousElementSibling.classList.add('top');
+        input.previousElementSibling.classList.add('focus');
+        input.parentNode.classList.add('focus');
+    };
+
+    input.onblur = function(){
+        if(input.value.trim().length == 0){
+            input.previousElementSibling.classList.remove('top');
+        }
+        input.previousElementSibling.classList.remove('focus');
+        input.parentNode.classList.remove('focus');
+    }
+});
